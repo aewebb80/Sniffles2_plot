d548666f2bbf4180c5b738dc9bb951a3cdaaaafc
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2025-10-24 12:18:25 -0400

    update main call to fix entry point

--- src/sniffles2_plot/__main__.py
+++ src/sniffles2_plot/__main__.py
@@ -4,6 +4,10 @@ Created on Mon Jun  5 10:48:59 2023
 
 @author: HGSC
 """
-from sniffles2_plot.cli import entry_point 
-if __name__ == "__main__":
+from sniffles2_plot.cli import entry_point
+
+def main():
     entry_point()
+
+if __name__ == "__main__":
+    main()
